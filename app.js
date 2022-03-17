0ea40260da516df18313cb9f308c8e014aff536d
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-17 16:31:44 -0400

    Env setup

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// Set up mongoose connection
+const mongoose = require('mongoose')
+const mongoDB = 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
