d743ef7038d04539a6b70fe41bae6a8a76e7c780
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-18 10:40:48 -0400

    Add MongoDB models. Populate database
    Add mongoose epxress library. Add models and schemas for authors,
    books, genres, and bookinstances. Ran populatedb script to fill
    database with sample data.

---
+++ models/genre.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Genre = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
+
+Genre.virtual("url").get(() => "/catalog/genre" + this._id);
+
+module.exports = mongoose.model("Genre", Genre);
